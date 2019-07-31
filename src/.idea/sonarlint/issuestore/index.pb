5a40934afa9234c854201ab6943aa8d161ef69da
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-31 12:44:56 +0530

    adding modified controller file

---
+++ src/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+_
+/main/java/com/stackroute/config/UserConfig.java,8/d/8d4aa8b138baa87eca17492bd379c71a8f1358f1
+d
+4main/java/com/stackroute/config/UserInitializer.java,3/b/3b159ef74006fe5ea6dcf11bce5bb23e10ead3ae
+g
+7main/java/com/stackroute/controller/UserController.java,4/3/43e3f6e6e6a4452fc369d37a0c8fa8aeaa744a50
+R
+"main/java/com/stackroute/User.java,2/3/233568f8ed9756734cc0c4947df7eec044fa7046
+U
+%main/webapp/WEB-INF/views/display.jsp,4/d/4d8af0a4ff91e2272fb206e1339bc29f3e5892e3
--- src/main/java/com/stackroute/controller/UserController.java
+++ src/main/java/com/stackroute/controller/UserController.java
@@ -2,12 +2,13 @@ package com.stackroute.controller;
 
 import com.stackroute.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 
 @Controller
@@ -15,14 +16,13 @@ public class UserController {
 
     @RequestMapping("/")
     public String returnMessage() {
+
         return "index";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public ModelAndView userLogin(HttpServletRequest request, HttpServletResponse response)
+    public ModelAndView success(@RequestParam("username")String username, @RequestParam("password")String password, ModelMap map)
     {
-        String username =request.getParameter("username");
-        String password = request.getParameter("password");
 
         User user = new User();
         user.setUsername(username);
